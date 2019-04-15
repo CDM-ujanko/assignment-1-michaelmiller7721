c19dbaabb64b27007267cd827460c921912b129c
Michael <michaelmiller7721@yahoo.com>
2019-04-14 21:59:42 -0500

    Commit more filesgit statusgit statusgit status

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("New file Yipppeee!!!!");
+console.log($)
+$(document).ready(function(){
+    // Handler for. ready() called.
+    console.log("I am ready.")
+});
+alert("Welcome to my first assignment");
